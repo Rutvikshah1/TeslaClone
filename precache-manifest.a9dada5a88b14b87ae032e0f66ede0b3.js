3d9f9a12d2e6343ac600d4663d1a9a0cb5febf16
Rutvik shah <rutvikshah245@gmail.com>
2020-09-18 22:11:45 +0530

    Updates

---
+++ precache-manifest.a9dada5a88b14b87ae032e0f66ede0b3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1791cc1fd9be83eada8a",
+    "url": "/Rutvikshah1/TeslaClone/static/css/main.7e8650b1.chunk.css"
+  },
+  {
+    "revision": "1791cc1fd9be83eada8a",
+    "url": "/Rutvikshah1/TeslaClone/static/js/main.052d7249.chunk.js"
+  },
+  {
+    "revision": "b8a3d6e4c912db945985",
+    "url": "/Rutvikshah1/TeslaClone/static/js/runtime~main.a3cc2a28.js"
+  },
+  {
+    "revision": "83aeb11b3c25f4530d61",
+    "url": "/Rutvikshah1/TeslaClone/static/js/2.1cc68593.chunk.js"
+  },
+  {
+    "revision": "f5ca9e2af6a6b2c4a18d09a3dc04968b",
+    "url": "/Rutvikshah1/TeslaClone/static/media/01_Main_Hero_Mobile.f5ca9e2a.jpg"
+  },
+  {
+    "revision": "bda0915111e37336936e72911b594ece",
+    "url": "/Rutvikshah1/TeslaClone/static/media/01_Main_Hero_Desktop.bda09151.jpg"
+  },
+  {
+    "revision": "4205d81010cc56336ad9fbe42d5fa044",
+    "url": "/Rutvikshah1/TeslaClone/static/media/carlogo.4205d810.png"
+  },
+  {
+    "revision": "2496895d5077c053325619c7aa5e9ef0",
+    "url": "/Rutvikshah1/TeslaClone/static/media/02_Desktop.2496895d.jpg"
+  },
+  {
+    "revision": "a93a6cd5dba73fee2f5e9554bba1d704",
+    "url": "/Rutvikshah1/TeslaClone/static/media/03_Desktop.a93a6cd5.jpg"
+  },
+  {
+    "revision": "22a0e9656dc07bdae92b92f085180aa2",
+    "url": "/Rutvikshah1/TeslaClone/static/media/04_Desktop.22a0e965.jpg"
+  },
+  {
+    "revision": "b01c0571e93f21ffefe3f02a6673e21a",
+    "url": "/Rutvikshah1/TeslaClone/static/media/05_Desktop.b01c0571.jpg"
+  },
+  {
+    "revision": "f3e00b2b2d4ebbd6aa2d5ab26057bb4e",
+    "url": "/Rutvikshah1/TeslaClone/static/media/08_Desktop.f3e00b2b.jpg"
+  },
+  {
+    "revision": "67f26a4d99a99108c064a84bf3625729",
+    "url": "/Rutvikshah1/TeslaClone/static/media/10_Desktop.67f26a4d.jpg"
+  },
+  {
+    "revision": "cea7ddcd44b1deac27298b4fe317a09b",
+    "url": "/Rutvikshah1/TeslaClone/index.html"
+  }
+];
